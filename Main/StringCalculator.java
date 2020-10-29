a66367cebea8ffb1dba00782a1d280afdecbc887
vinita04 <vinita04>
2020-10-29 15:10:50 +0530

    Support different delimiters

--- Main/StringCalculator.java
+++ Main/StringCalculator.java
@@ -1,16 +1,57 @@
-public class StringCalculator {
-	public static int Add(String numbers){
-		if(numbers==null || numbers.isEmpty()){
-			return 0;
-		}
-		else{
-			String[] chuncks = numbers.split(",|\n");
-			int total = 0;
-			for (String n : chuncks) {
-				int num = Integer.parseInt(n);
-				total += num;            
-			}
-			return total;
-		}
-	}
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import static java.util.Arrays.asList;
+
+class StringCalculator {
+	private static final String PIPE_DELIMITER = "|";
+    public static final int START_INDEX_OFFSET = 3;
+    private Pattern customDelimitersValidator = Pattern.compile("//(.*)\n(.*)");
+    private Matcher customDelimitersMatcher;
+    private int startIndex;
+	
+    public int Add(String numbers) {
+        if (numbers==null ||numbers.isEmpty()) {
+            return 0;
+        }
+        List<String> numbersList = getNumbers(numbers);
+        //System.out.println(sumArray(numbersList));
+        return sumArray(numbersList);
+    }
+
+    private int sumArray(List<String> numbersList) {
+        return numbersList.stream().mapToInt(Integer::parseInt).sum();
+    }
+
+	List<String> getNumbers(String string) {
+        String delimiters = getDelimiters(string);
+        return splitNumbers(string, delimiters);
+    }
+
+    private List<String> splitNumbers(String string, String delimiters) {
+        if (areCustomDelimitersValid(string)) {
+            string = string.substring(startIndex);
+        }
+        return asList(string.split(delimiters));
+    }
+
+    private String getDelimiters(String string) {
+        String delimiters = ",|\n";
+        if (areCustomDelimitersValid(string)) {
+            delimiters += PIPE_DELIMITER + getCustomDelimiters();
+        }
+        return delimiters;
+    }
+	
+	private String getCustomDelimiters() {
+        String customDelimiters = customDelimitersMatcher.group(1);
+        startIndex = customDelimiters.length() + START_INDEX_OFFSET;
+        return customDelimiters;
+    }
+
+    private boolean areCustomDelimitersValid(String string) {
+        customDelimitersMatcher=customDelimitersValidator.matcher(string);
+        return customDelimitersMatcher.matches();
+    }
 }
--- Test/StringCalculatorTest.java
+++ Test/StringCalculatorTest.java
@@ -1,39 +1,52 @@
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.Before;
+import org.junit.Rule;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 
 public class StringCalculatorTest {
 	private StringCalculator calculator;
+	
 	@Before
     public void initialize() {
         calculator = new StringCalculator();
     }
-
+	
+	@Rule
+    public ExpectedException thrown = ExpectedException.none();
+	
     @Test
     public void emptyStringShouldReturn0() {
         assertEquals(calculator.Add(""), 0);
     }
 
-    @Test
-    public void numberStringShouldReturnSameNumber() {
+	@Test
+	public void numberStringShouldReturnSameNumber() {
         assertEquals(calculator.Add("1"), 1);
     }
 
     @Test
     public void twoNumbersShouldBeAdded() {
-    	assertEquals(3, calculator.Add("1,2"));	
+    	assertEquals(3, calculator.Add("1,2"));
     }
     
     @Test
     public void moreDigitsSupported() {
         assertEquals(45, calculator.Add("1,2,3,4,5,6,7,8,9"));
     }
-
-	@Test
+    
+    @Test
     public void numbersNewlineDelimitedShouldBeSummed() {
         assertEquals(calculator.Add("1\n2,3"), 6);
         assertEquals(calculator.Add("11\n13\n2"), 26);
     }
+    
+    @Test
+    public void numbersDelimitedSeparatedShouldBeSummed() {
+        assertEquals(3, calculator.Add("//;\n1;2"));
+    }
+    
 }
